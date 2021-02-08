516812fa6a923915db225469430e06f4cca58b24
Dzmitry Rudak <dzm.rudak@gmail.com>
2021-02-08 02:01:03 +0100

    Calculating of the number length has been added

--- src/by/rudakd/lesson2/task1/Task1.java
+++ src/by/rudakd/lesson2/task1/Task1.java
@@ -6,7 +6,15 @@ public class Task1 {
     public static void main(String[] args) {
         int convertNumber = 1234;
         int div = 1000, fact = 1;
-        int number[] = new int[4];
+        int count = 0;
+        int temp = convertNumber;
+        if(temp > 0) {
+            while(temp > 0) {
+                temp /= 10;
+                count++;
+            }
+        } else System.out.println("Число равно 0!");
+        int number[] = new int[count];
         int authNum = 0;
         for (int i = 0; i < number.length; i++){
             number[i] = convertNumber/div % 10 * fact;
